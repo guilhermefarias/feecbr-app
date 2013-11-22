1c414b4c874f57f340c19a5a2d0f2f42012654ce
Guilherme Farias <guilherme@guiky.com.br>
2013-11-22 12:34:06 -0300

    Loading handlebars and showing firsts templates

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require('express');
+var exphbs = require('express3-handlebars');
 var app = express();
 
-app.get('/', function(req, res){
-	res.send('hey, this work!!');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function(request, response, next){
+	response.render('home');
 });
 
-app.listen(3000,function(){
+app.get('/photos', function(request, response, next){
+	response.render('photos');
+});
+
+app.listen(3000,function(request, response, next){
  	console.log('App serving on port 3000');
 });
 
